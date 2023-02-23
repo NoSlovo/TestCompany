abcec6769f19f9203968d389e1af16fdc99642c5
NoSlovo <88000429+NoSlovo@users.noreply.github.com>
2023-02-23 19:19:26 +0500

    AnimatorSettings

---
+++ TestCompany/Assets/GameResource/Sctipts/PlayerAnimator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace GameResource.Sctipts
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        
+        private readonly int _idle = Animator.StringToHash("Idle");
+        private readonly int _fastRun = Animator.StringToHash("FastRun");
+        private readonly int _iRun = Animator.StringToHash("IRin");
+
+        public void Move() => _animator.SetBool(_iRun, true);
+        public void StopMove() => _animator.SetBool(_iRun, false);
+
+    }
+}
