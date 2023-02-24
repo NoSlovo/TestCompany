a01ce009c12ba18f07ecc17a6109d25f597ea074
NoSlovo <88000429+NoSlovo@users.noreply.github.com>
2023-02-24 20:15:35 +0500

    FireBollSetings

--- TestCompany/Assets/FireBoll.cs
+++ TestCompany/Assets/FireBoll.cs
@@ -1,8 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FireBoll : MonoBehaviour
 {
-    
+     private float _spead = 8f;
+     private float _duraticon = 5f;
+     private float _timeCount;
+
+     private void FixedUpdate()
+     {
+          _timeCount += Time.deltaTime;
+          transform.position += Vector3.left * (_spead * Time.deltaTime);
+          
+          if (_timeCount >= _duraticon)
+                    Destroy(this.gameObject);
+     }
 }
